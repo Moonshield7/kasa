6b06e4bb3c38648499c0d29ff1d15f3f7802f910
Moonshield7 <fannie.p@hotmail.fr>
2023-10-13 12:18:52 +0200

    CRLF to LF

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
 import colors from '../../utils/style/colors';
 import picture1 from '../../utils/assets/homeimg.png';
-import picture2 from '../../utils/assets/aboutimg.png';
 
+const text = 'Chez vous, partout et ailleurs';
 const Background = styled.header`
   width: 90%;
   height: 223px;
+  background: url(${picture1});
   background-repeat: no-repeat;
   background-size: cover;
   background-position: center;
@@ -13,11 +14,6 @@ const Background = styled.header`
   border-radius: 15px;
   margin: auto;
   margin-top: 50px;
-  @media screen and (max-width: 430px) {
-    height: 111px;
-    margin-top: 25px;
-    margin-bottom: 25px;
-  }
 `;
 
 const Title = styled.h1`
@@ -30,39 +26,12 @@ const Title = styled.h1`
   display: flex;
   align-items: center;
   justify-content: center;
-  @media screen and (max-width: 430px) {
-    font-size: 24px;
-    padding: 25px;
-    justify-content: left;
-  }
-`;
-const LignReturn = styled.br`
-  @media screen and (min-width: 430px) {
-    display: none;
-  }
 `;
 
 function Banner() {
-  const activePage = window.location.href;
-  const styles = {
-    image: {
-      backgroundImage:
-        activePage === 'http://localhost:3000/about' ? `url(${picture2})` : `url(${picture1})`,
-    },
-  };
   return (
-    <Background style={styles.image}>
-      <Title>
-        {activePage === 'http://localhost:3000/about' ? (
-          ''
-        ) : (
-          <span>
-            Chez vous,
-            <LignReturn />
-            partout et ailleurs
-          </span>
-        )}
-      </Title>
+    <Background>
+      <Title>{text}</Title>
     </Background>
   );
 }
